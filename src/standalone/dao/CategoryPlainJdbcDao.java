4cde446d75f43cb6223b9362ce13279b388bd61c
rayapudisandeep <rayapudisandeep@975e6314-f4f4-11de-bd8e-1f356e038253>
2010-01-12 16:16:00 +0000

    [java] deleteAll:Wed Jan 13 00:13:57 SGT 2010      [java] Wed Jan 13 00:13:58 SGT 2010      [java] in executeBatch:Wed Jan 13 00:14:01 SGT 2010      [java] Wed Jan 13 00:14:29 SGT 2010      [java] deleteAll:Wed Jan 13 00:14:32 SGT 2010      [java] Wed Jan 13 00:14:32 SGT 2010      [java] executing preparedStatement 100 times in loop:Wed Jan 13 00:14:35 SGT 2010      [java] Wed Jan 13 00:15:04 SGT 2010

---
+++ src/standalone/dao/CategoryPlainJdbcDao.java
@@ -0,0 +1,137 @@
+package standalone.dao;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.Properties;
+import standalone.beans.Category;
+
+public class CategoryPlainJdbcDao implements CategoryDao {
+
+	java.util.Properties dbProperties;
+	String url, username, password, className;
+
+	public CategoryPlainJdbcDao()
+	{
+		dbProperties = new Properties();
+		try {
+			dbProperties.load(new FileInputStream("resources/jdbc.properties"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		url = dbProperties.getProperty("jdbc.url");
+		username = dbProperties.getProperty("jdbc.username");
+		password = dbProperties.getProperty("jdbc.password");
+		className = dbProperties.getProperty("jdbc.driverClassName");
+		
+		try {
+			Class.forName(className);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+	@Override
+	public int addCategory(long id, String categoryName, String description) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int[] batchInsert() {
+		
+		try {
+
+			Connection conn = DriverManager.getConnection(url, username, password);
+			PreparedStatement ps = conn.prepareStatement("insert into au_category values (?, ?, ?, ?)");
+			for(int i=0;i<BATCH_INSERT_SIZE; i++)
+			{
+				ps.setInt(1, i);
+				ps.setString(2, "categoryName");
+				ps.setString(3, "descriptionName");
+				ps.setInt(4, 0);
+				
+				ps.addBatch();
+			}
+			
+			System.out.println("in executeBatch:" + new Date(System.currentTimeMillis()));
+			int[] result = ps.executeBatch();
+			System.out.println(new Date(System.currentTimeMillis()));
+			ps.close();
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public int deleteAll() {
+		// TODO Auto-generated method stub
+		boolean result = false;
+		try {
+			Connection conn = DriverManager.getConnection(url, username, password);
+			PreparedStatement ps = conn.prepareStatement("delete from au_category");		
+			System.out.println("deleteAll:" +new Date(System.currentTimeMillis()));
+			result = ps.execute();
+			System.out.println(new Date(System.currentTimeMillis()));
+			ps.close();
+			conn.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result==false?0:1;
+	}
+	
+	public void batchInsertInLoop()
+	{
+		try {
+
+			Connection conn = DriverManager.getConnection(url, username, password);
+			
+			System.out.println("executing preparedStatement 100 times in loop:" + new Date(System.currentTimeMillis()));
+			for(int i=0;i<BATCH_INSERT_SIZE; i++)
+			{
+				PreparedStatement ps = conn.prepareStatement("insert into au_category values (?, ?, ?, ?)");
+				ps.setInt(1, i);
+				ps.setString(2, "categoryName");
+				ps.setString(3, "descriptionName");
+				ps.setInt(4, 0);
+				
+				ps.execute();
+				ps.close();
+			}
+			System.out.println(new Date(System.currentTimeMillis()));
+			
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public int deleteCategory(long id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Category getCategory(long id, String categoryName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
