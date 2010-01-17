429bf2143fe0e2b81cb2c222ca31050477303da5
rayapudisandeep <rayapudisandeep@975e6314-f4f4-11de-bd8e-1f356e038253>
2010-01-17 16:12:07 +0000

    Trying to get web working...
    Could get home page loaded. but trying to do the deployment with Ant war files.... WAR task seems to require more understanding.. i think i should try automating the deployment first.. then i can try war file thingy...

---
+++ src/com/roadrantz/service/RantService.java
@@ -0,0 +1,14 @@
+package com.roadrantz.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RantService {
+	public List getRecentRants()
+	{
+		ArrayList<Integer> list = new ArrayList();
+		list.add(Integer.parseInt("1"));
+		list.add(Integer.parseInt("2"));
+		return list;
+	}
+}
---
+++ webapp/src/home.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html" %>
+
+<html>
+<head><title>Rantz</title></head>
+<body>
+<h2>Welcome to RoadRantz!</h2>
+<h3>Recent rantz:</h3>
+<ul>
+</ul>
+</body>
+</html>
