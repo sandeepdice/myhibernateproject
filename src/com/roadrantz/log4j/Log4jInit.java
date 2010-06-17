27d164112431db6f4ab91222f9045595dc294e29
rayapudisandeep <rayapudisandeep@975e6314-f4f4-11de-bd8e-1f356e038253>
2010-06-17 05:32:20 +0000

    back to work.................. trying to get log4j working...

--- src/com/roadrantz/log4j/Log4jInit.java
+++ src/com/roadrantz/log4j/Log4jInit.java
@@ -2,6 +2,8 @@ package com.roadrantz.log4j;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,16 +13,23 @@ import java.io.IOException;
 
 public class Log4jInit extends HttpServlet {
 
-	protected final Log logger = LogFactory.getLog(getClass());
+//	protected final Log logger = LogFactory.getLog(getClass());
+	protected Logger logger;
 	
   public
   void init() {
+	System.out.println("initializing log4j");
     String prefix =  getServletContext().getRealPath("/");
     String file = getInitParameter("log4j-init-file");
     // if the log4j-init-file is not set, then no point in trying
     if(file != null) {
     	PropertyConfigurator.configure(prefix+file);
     }
+//    logger.info("log4j initialized");
+    logger = LogManager.getLogger("file");
+    System.out.println(logger);
+    logger.trace("log4j initialized");
+    logger.debug("log4j initialized");
     logger.info("log4j initialized");
   }
 
