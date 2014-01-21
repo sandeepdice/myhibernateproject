69a6b7e3a45363d563dad243e8fa79a6dace5222
rayapudisandeep@gmail.com <rayapudisandeep@gmail.com@975e6314-f4f4-11de-bd8e-1f356e038253>
2014-01-21 02:48:04 +0000

    included f2f round questions.

--- learning/paypal.java
+++ learning/paypal.java
@@ -37,4 +37,49 @@ class B extends A
     12. how's it passed to backend
     13. how's authorization done in application
     14. can variables in interface definition have static modifier? yes. they are implicitly public static final
+    
+    Round 2 - 20 jan 2014
+    Print 1/n 23/n 456/n 78910/n
+A big file contains dept and age of employees. Calculate avg age by dept.. Use data structures
+What r two design problems you solved at work
+What's your work on web services
+
+Round 3 - 20 jan 2014
+Program to swap instance variables of two objects
+
+String as="too";
+String as2="too";
+String sb=new String("too");
+System.out.println(as==sb);
+System.out.println(as==as2);
+System.out.println(as.equals(sb));
+
+Write examples of factory, singleton, builder
+Diff btwn stub n mock
+What's mock frwrk u used for ut
+Static class
+Overriding n loading
+Overrides annotations
+who gets user stories in scrum
+what is your day to day activities
+do you have a QA team
+
+Round 4 - 20 jan 2014
+What's d recent thing u learned
+What r different types of program lang
+What do u think abt node.js
+World is moving towards js..
+Simple db normalization scenarios
+Problem on not exists clause
+How r user stories estimations quantified? Fibnoucii ( thEy call it poker)
+Velocity?
+WAP fibnoucci using recursion and without
+How do u design an application that lists recent ten records from table contains billion records that receiving millions of requests every minute
+Caching?
+Where does caching layer go
+How cache gets synced with db updates
+Publish subscribe mechanism
+Have u heard of materialized views
+What's cache key..
+Why shud pp hire you
 */
